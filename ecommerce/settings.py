4d84b43122762e0477cc7e6a4ef2ee50729ad5cb
camilo177 <juanc.lopezca@campusucc.edu.co>
2024-05-23 15:07:59 -0500

    database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': '<db>',
+        'USER': '<camilo>',
+        'PASSWORD': '<Nairocontador17>',
+        'HOST': 'bikefinal.database.windows.net',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'MARS_Connection': True,
+        },
     }
 }
 
