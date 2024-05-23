57a368469d2521c32d13cdcc8b762682f1b010f8
camilo177 <juanc.lopezca@campusucc.edu.co>
2024-05-23 11:51:21 -0500

    hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bikecommerce.azurewebsites.net']
+ALLOWED_HOSTS = ['bikecommerce.azurewebsites.net', '127.0.0.1', 'localhost']
 
 
 # Application definition
